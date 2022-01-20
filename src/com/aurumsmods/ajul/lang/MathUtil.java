4852e61dd4823a60f40b5559b5d99f99cda19822
SunakazeKun <nintendo.rpgforum@gmail.com>
2022-01-20 18:36:02 +0100

    new BitUtil functions

--- src/com/aurumsmods/ajul/lang/MathUtil.java
+++ src/com/aurumsmods/ajul/lang/MathUtil.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2021 Aurum.
+ * Copyright 2022 Aurum.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
--- src/com/aurumsmods/ajul/util/BitUtil.java
+++ src/com/aurumsmods/ajul/util/BitUtil.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2021 Aurum.
+ * Copyright 2022 Aurum.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -73,7 +73,7 @@ public final class BitUtil {
     }
     
     /**
-     * If {@code state} is true, the specified bit in {@code val} will be set. Otherwise, the bit will be cleared. The resulting
+     * If {@code state} is true, the specified bit in {@code val} will be set, otherwise, the bit will be cleared. The resulting
      * value will be returned.
      * @param val the value to update the bit in.
      * @param bit the bit to be updated.
@@ -82,5 +82,28 @@ public final class BitUtil {
      */
     public static int update(int val, int bit, boolean state) {
         return state ? set(val, bit) : clear(val, bit);
+    }
+    
+    /**
+     * Tests the set bits in {@code val} against the specified bitmask. Returns {@code true} if all of them are set,
+     * {@code false} if not.
+     * @param val the value to check the bits in.
+     * @param mask the bitmask.
+     * @return {@code true} if all specifies bits are set, {@code false} if not.
+     */
+    public static boolean testWithMask(int val, int mask) {
+        return (val & mask) != 0;
+    }
+    
+    /**
+     * If {@code state} is true, the result of {@code val} | {@code mask} will be returned, otherwise, the result of 
+     * {@code val} {@literal &} ~{@code mask} will be returned.
+     * @param val the value to update.
+     * @param mask the bitmask.
+     * @param state declares whether to set ({@code true}) or clear ({@code false}) the bits.
+     * @return the updated result.
+     */
+    public static int updateWithMask(int val, int mask, boolean state) {
+        return state ? val | mask : val & ~mask;
     }
 }
